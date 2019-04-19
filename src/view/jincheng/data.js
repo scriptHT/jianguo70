215920d74edcb3f7cd84c7cdd2303354e657f713
helinyun <615953137@qq.com>
2019-04-19 21:13:18 +0800

    h

---
+++ src/view/jincheng/data.js
@@ -0,0 +1,5 @@
+pp={
+    p1:'dsafdsafdsafdsafdsafdsafdsaf',
+    p2:'13456789',
+    p3:'7890'
+}
--- src/view/jincheng/index.js
+++ src/view/jincheng/index.js
@@ -1,6 +1,8 @@
 import React,{Component} from "react";
 import AnimatedWrapper from "../AnimatedWrapper";
-import "./jincheng.css"
+import "./jincheng.css";
+import $ from "jquery";
+import "./coolShow.css";
 import img1 from "./img/1.jpg";
 import img2 from "./img/2.jpg";
 import img3 from "./img/3.jpg";
@@ -10,10 +12,18 @@ import img6 from "./img/6.jpg";
 import img7 from "./img/7.jpg";
 import img8 from "./img/8.jpg";
 import img9 from "./img/9.jpg";
-
+var ppp=['asdfsa','dsajhfgiohdsagoi','s1234567'];
 class  JinchengComponent extends Component{
     constructor(props){
         super(props);
+        this.state={
+            id:'',
+            imgSrc:'',
+            name:'trans_fade_image_click',
+            pshow:'none',
+            content:''
+        }
+        const Imgsrc=['./img/1.jpg']
     }
     rotate3D = () => {
         let wrap = document.getElementById("wrap")
@@ -27,62 +37,176 @@ class  JinchengComponent extends Component{
             
         })
         var newX,newY,lastX,lastY,minusX,minusY,rotX = -20 ,rotY = 0;
-        document.onmousedown = function  (e) {
-            //鼠标点击事件
-            //最开始的旧值等于第一次点击时产生的XY
-            lastX = e.clientX;
-            lastY = e.clientY
-            this.onmousemove = function  (e) {
-            // 鼠标移动事件
-            //最新的XY值
-            newX = e.clientX;
-            newY = e.clientY;
-            //计算差值
-            minusX = newX - lastX;
-            minusY = newY - lastY;
-            //差值累加成旋转距离
-            rotX -= minusY*0.15;
-            rotY += minusX*0.15;
-            wrap.style.transform = "rotateX("+rotX+"deg)  rotateY("+rotY+"deg)";
-            //计算之后，新值就变成旧值
-            lastX = newX;
-            lastY = newY;
+    
 
-            }
-            this.onmouseup = function  (e) {
-                // 鼠标松开事件
-                this.onmousemove = null;//清除鼠标移动事件
-            }
-        }
+
         setInterval(()=> {
             rotY+=0.15;
             wrap.style.transform = "rotateX("+rotX+"deg) rotateY("+rotY+"deg)";
         },10)
       
     }
+//    imgCS=(e)=>{
+
+//         $('#wrap').find('img').on('click',function(){
+//             alert(4);
+//             $(this).stop();
+//             $('#coolShow b').remove();
+//             /*     生成图片的载体    */
+//             for (var i = 0;i<($("#coolShow").height()/10);i++) $('#coolShow').append('<b></b>');
+//             console.log($("#coolShow").height());
+//             /*     图片显示特效    */
+//             var psn = 0;
+//             var imgId = $(this).children().data('img');
+//             $('#coolShow b').each(function(){
+//                 $(this).css({
+//                     opacity:0,
+//                     backgroundPosition:"0 "+psn+"px",
+//                     backgroundImage:'url("'+options.id+'")'
+//                 });
+//                 psn -= 10;
+//             });
+//             var time = 0;
+//             $('#coolShow b').each(function(){
+//                 $(this).delay(time).animate({opacity:"1"},500);
+//                 time += 40;
+//             });
+//         });
+   
+//    }
+
+
+
+  fadeIn =(props)=>{
+    $.fn.coolShow = function(options){
+        // alert(1);
+		// /*     初始化参数     */
+		// var defaults = {
+		// 	imgSrc:'',
+		// 	speed:50
+        // };
+	
+		 var inita=	function(){
+                alert(2);
+				$('#wrap').find('img').on('click',function(){
+                    alert(4);
+					$(this).stop();
+					$('#coolShow b').remove();
+					/*     生成图片的载体    */
+                    for (var i = 0;i<($("#coolShow").height()/10);i++) $('#coolShow').append('<b></b>');
+                    console.log($("#coolShow").height());
+					/*     图片显示特效    */
+					var psn = 0;
+					var imgId = $(this).children().data('img');
+					$('#coolShow b').each(function(){
+						$(this).css({
+							opacity:0,
+							backgroundPosition:"0 "+psn+"px",
+                            backgroundImage:'url("'+options.id+'")'
+						});
+						psn -= 10;
+					});
+					var time = 0;
+					$('#coolShow b').each(function(){
+						$(this).delay(time).animate({opacity:"1"},500);
+						time += 40;
+					});
+				});
+			}
+        
+
+		/*     代码开始     */
+		return this.each(function(){
+            alert(3);
+			/*     接收传参     */
+		
+			/*     生成按钮的载体    */
+         
+			/*     特效开始    */
+            inita();
+		});
+    };
+  }
+
+	
+
+    imgShow=(e)=>{  
+        this.setState({
+            id:e.target.id,
+            imgSrc:e.target.src,
+            name:'',
+            pshow:'block',
+            content:ppp[this.state.id]
+        }) 
+        let ImgSrcs=  e.target.src;
     
+            $('#coolShow b').remove();
+            /*     生成图片的载体    */
+            for (var i = 0;i<($("#coolShow").height()/10);i++) $('#coolShow').append('<b></b>');
+            console.log($("#coolShow").height());
+            /*     图片显示特效    */
+            var psn = 0;
+            var imgId = $(this).children().data('img');
+            $('#coolShow b').each(function(){
+                $(this).css({
+                    opacity:0,
+                    backgroundPosition:"0 "+psn+"px",
+                    backgroundImage:'url("'+ImgSrcs+'")'
+                });
+                psn -= 10;
+            });
+            var time = 0;
+            $('#coolShow b').each(function(){
+                $(this).delay(time).animate({opacity:"1"},500);
+                time += 40;
+            });
+       
+        // $('#coolShow').coolShow({
+        //     speed: 40,
+        //     id:this.state.imgSrc
+        // });   
+        // alert(6); 
+       
+}
+
     componentDidMount(){
         this.rotate3D();
+        this.fadeIn();
+        $('#coolShow').coolShow({
+            speed: 40,
+            id:this.state.imgSrc
+        });   
     }
     render(){
         return(
             <div id="mainWrap">
-                <div id="perspective">
-                    <div id="wrap">
-                        <img alt="" src={img1}></img>
-                        <img alt="" src={img2}></img>
-                        <img alt="" src={img3}></img>
-                        <img alt="" src={img4}></img>
-                        <img alt="" src={img5}></img>
-                        <img alt="" src={img6}></img>
-                        <img alt="" src={img7}></img>
-                        <img alt="" src={img8}></img>
-                        <img alt="" src={img9}></img>
+                <div id="perspective"    onClick={(e)=>this.imgShow(e)} >
+                    <div id="wrap" >
+                        <img id="0" alt="" src={img2}></img>
+                        <img id="1" alt="" src={img3}></img>
+                        <img id="2" alt="" src={img4}></img>
+                        <img id="3" alt="" src={img5}></img>
+                        <img id="4" alt="" src={img6}></img>
+                        <img id="5" alt="" src={img7}></img>
+                        <img id="6" alt="" src={img8}></img>
+                        <img id="7" alt="" src={img9}></img>
                     </div>
                 </div>
+                <div id="wrap1" >
+                <p style={{display:this.state.pshow}} id="showp">{this.state.content}</p>
+                {/* <img id="show" alt="" className={this.state.name} src={this.state.imgSrc}></img> */}
+                <div className="main">
+                <div id="coolShow"></div>
+                <div id="handBar">   
+ </div>
+   
+    </div>
+
+                    </div>
             </div>
         )
     }
 }
+
 const Jincheng = AnimatedWrapper(JinchengComponent);
 export default  Jincheng;
